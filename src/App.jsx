76b5b78d890a8ad2ac31fb4e806ed8ddf5048f4a
iznor <iznor.hucuev9507@gmail.comm>
2022-12-13 13:30:10 +0300

    сделал компоненты футера и хедера

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.scss";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="app">
-      
+       <Header/>
+      <Footer/>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React from "react";
+import style from "./Header.module.scss";
+import toyStream from "./photos/toyStream.png";
+ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className={style.header}>
+      <div>
+        <div className={style.toyStream}>
+          <img className={style.imgToyStream} src={toyStream} alt="фото header" />
+        </div>
+      </div>
+      <div className={style.textMenu}>
+        <div className={style.links}>
+         <Link to='/main'>
+         <button className={style.BtnLink}>Главная</button>
+         </Link>
+         <Link to='/compain'>
+         <button className={style.BtnLink}>Компания</button>
+         </Link>
+         <Link to='/games'>
+         <button className={style.BtnLink}>Игры</button>
+         </Link>
+         <Link to='/сareer'>
+         <button className={style.BtnLink}>Карьера</button>
+         </Link>
+         <Link to='/news'>
+         <button className={style.BtnLink}>Новости</button>
+         </Link>
+         <Link to='/contacts'>
+         <button className={style.BtnLink}>Контакты</button>
+         </Link>
+        </div>
+
+        <div className={style.language}>
+          <p className={style.btnLangiage}>ENG</p>
+        </div>
+      </div>
     </div>
   );
 };
