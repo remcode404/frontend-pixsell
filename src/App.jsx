ddd009c52f87ea2cdf7e0e4f33bd25800ea475ec
Blacksider1 <adamtsurov123@gmail.com>
2022-12-13 16:29:15 +0300

    Сделал ничего

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import "./App.scss";
-import MainPageVacancy from "./pages/Vacancy/MainPageVacancy";
 
 function App() {
   return (
     <div className="app">
-      <MainPageVacancy />
+      
     </div>
   );
 }
