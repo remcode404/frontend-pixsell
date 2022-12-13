b22af044a7334ea4031823f67b7be1d350d299d3
JokerJok <dmehtiev@inbox.ru>
2022-12-13 14:21:41 +0300

    Накинул верстку главной страницы

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.scss";
+import MainPage from "./pages/MainPage/MainPage";
 
 function App() {
   return (
-    <div className="app">
-      
+    <div>
+      <MainPage />
     </div>
   );
 }
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React from "react";
+import style from "../MainPage/MainPage.module.scss";
+import horse from "../MainPage/files/Frame.png";
 
 const MainPage = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className={style.mainLeft}>
+        <div className={style.mainLeftText}>
+          Мы создаем мобильные игры, в которые ты играешь
+        </div>
+      </div>
+      <div className={style.mainRight}>
+        <div className={style.yellowBlock}></div>
+        <div className={style.yellowBlockImg}>
+          <img src={horse} alt="Конь" />
+        </div>
+      </div>
+    </>
   );
 };
 
