602e2a177ad40e32373a088c4b0b6a2cf079de97
JokerJok <dmehtiev@inbox.ru>
2022-12-15 11:45:42 +0300

    Страница описания вакансий

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import "./App.scss";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import GamePages from "./pages/Games/GameMain";
+import AboutVacancy from "./pages/Vacancy/AboutVacancy";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <GamePages />
+      <AboutVacancy />
       <Footer />
     </div>
   );
